438bc385a5a8bb4a9f8fae15a1f438b93d3c2fb3
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-06-26 13:19:27 -0500

    Test new deploy

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,7 +3,9 @@ import react from 'react'
 const About:React.FC = () =>{
     return(
         <div>
-            Hihii
+            Hihii 
+            <br>Extra commit to check if deploy is working
+            </br>
         </div>
     );
 };
