7cc0ee9ed8c58d5c89f94469afade1a6c6657a5c
rudyg <rudynoot123@gmail.com>
2025-07-05 23:59:51 -0500

    Netlify build reload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route, BrowserRouter } from "react-router-dom";
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -9,13 +9,14 @@ import Services from './pages/Services'
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/quote' element={<Quote />} />
-      <Route path='/services' element={<Services />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/quote' element={<Quote />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    
   );
 };
 
